4d9479563aea0d55d7496dc07a11dfc3fb80b735
iketaka1984 <iketaka3303@ezweb.ne.jp>
2020-02-06 14:03:33 +0900

    vmを見やすくしました

--- ex2/vm.py
+++ ex2/vm.py
@@ -1,18 +1,10 @@
 import re
 import sys
 import time
-import random
-import tkinter as tk
-from multiprocessing import Process, Value, Array, Lock, Manager, current_process, Semaphore
-
-pre=0
+from multiprocessing import Process, Value, Array, Lock
 codes=[]
 com=[]
 opr=[]
-parcom1=[]
-paropr1=[]
-parcom2=[]
-paropr2=[]
 stack=[]
 rstack=[]
 temp=[]
@@ -23,25 +15,21 @@ count_pc=0
 parflag=0
 args=sys.argv
 
+#push:スタックトップに値を積む
 def push(a,stack,top):
     stack.append(a)
-    #print("push "+str(a)+" at "+str(top)+"")
-    #print("push "+str(stack[top])+" in "+str(top)+"")
-    #print("stack",end=':')
-    #for i in range(0,len(stack),1):
-    #    print(str(stack[i]),end=',')
     return top+1
 
+#pop1:スタックトップから値をポップする
 def pop1(stack,top):
     t=stack[top-1]
-    #print("pop "+str(t)+"at "+str(top-1)+"")
     stack.pop()
     return (t,top-1)
 
-def executedcommand(mlock,stack,rstack,lstack,com,opr,pc,pre,top,rtop,ltop,address,value,parpath):
+#命令，被演算子の組を一つ受け取り実行する
+def executedcommand(stack,rstack,lstack,com,opr,pc,pre,top,rtop,ltop,address,value,parpath):
     if com==1:#push
         top=push(opr,stack,top)
-        #print(stack)
         pre=pc
         return (pc+1,pre,stack,top,rtop)
     elif com==2:#load
@@ -49,21 +37,16 @@ def executedcommand(mlock,stack,rstack,lstack,com,opr,pc,pre,top,rtop,ltop,addre
         c=value[opr]
         value.release()
         top=push(c,stack,top)
-        #print(stack)
         pre=pc
         return (pc+1,pre,stack,top,rtop)
     elif com==3:#store
         value.acquire()
         rstack[rtop.value]=(value[opr])
         rstack[rtop.value+1]=(parpath)
-        #print("rtop "+str(rtop.value)+"")
         rtop.value=rtop.value+2
         (stack[opr],top)=pop1(stack,top)
         value[opr]=stack[opr]
         value.release()
-        #print(stack)
-        #if bytes[pc+2]==3:
-        #    print("R = "+str(stack[3])+"")
         pre=pc
         return (pc+1,pre,stack,top,rtop)
     elif com==4:#jpc
@@ -77,30 +60,28 @@ def executedcommand(mlock,stack,rstack,lstack,com,opr,pc,pre,top,rtop,ltop,addre
         pc=opr-2
         return (pc+1,pre,stack,top,rtop)
     elif com==6:#op
-        if (opr)==0:
+        if (opr)==0:#'+'
             (c,top)=pop1(stack,top)
             (d,top)=pop1(stack,top)
             top=push(c+d,stack,top)
-        elif (opr)==1:
+        elif (opr)==1:#'*'
             (c,top)=pop1(stack,top)
             (d,top)=pop1(stack,top)
             top=push(c*d,stack,top)
-        elif opr==2:
+        elif opr==2:#'-'
             (c,top)=pop1(stack,top)
             (d,top)=pop1(stack,top)
             top=push(d-c,stack,top)
-        elif opr==3:
+        elif opr==3:#'>'
             (c,top)=pop1(stack,top)
             (d,top)=pop1(stack,top)
             if d>c:
                 top=push(1,stack,top)
             else:
                 top=push(0,stack,top)
-        elif opr==4:
+        elif opr==4:#'=='
             (c,top)=pop1(stack,top)
-            #print(c)
             (d,top)=pop1(stack,top)
-            #print(d)
             if d==c:
                 top=push(1,stack,top)
             else:
@@ -120,13 +101,11 @@ def executedcommand(mlock,stack,rstack,lstack,com,opr,pc,pre,top,rtop,ltop,addre
         pc=int(lstack[ltop.value])
         pc=pc-2
         ltop.value=ltop.value-1
-        #mlock.release()
         return (pc+1,pre,stack,top,rtop)
     elif com==9:#restore
         rtop.value=rtop.value-1
         value[opr]=int(rstack[rtop.value])
         rtop.value=rtop.value-1
-        #print(stack)
         pre=pc
         return (pc+1,pre,stack,top,rtop)
     elif com==0:#nop
@@ -143,9 +122,9 @@ def execution(mode,lock,mlock,command,opr,start,end,stack,address,value,tablecou
     top=len(stack)
     if args[2]=='f':
         while pc<end:
+            #現在のプロセスに鍵がかかっているか確認
             if parpath!=0:
                lock.acquire()
-            #if you want to measure time,comment out
             if args[3]!='q':
                 if command[pc]==1:
                     command1='ipush'
@@ -165,10 +144,12 @@ def execution(mode,lock,mlock,command,opr,start,end,stack,address,value,tablecou
                     command1='  par'
                 print("~~~~~~~~Process"+str(parpath)+" execute~~~~~~~~")
                 print("pc = "+str(pc+1)+"   command = "+command1+"   operand = "+str(opr[pc])+"")
-            (pc,pre,stack,top,rtop)=executedcommand(mlock,stack,rstack,lstack,command[pc],opr[pc],pc,pre,top,rtop,ltop,address,value,parpath)
+            #コマンドを実行する関数に処理を渡す
+            (pc,pre,stack,top,rtop)=executedcommand(stack,rstack,lstack,command[pc],opr[pc],pc,pre,top,rtop,ltop,address,value,parpath)
             if args[3]!='q':
                 print("executing stack:       "+str(stack[:])+"")
                 print("shared variable stack: "+str(value[0:tablecount])+"")
+            #表示モードによってプロセスの鍵の管理の仕方が違う
             if parpath!=0:
                 if mode=='2':
                     lock.acquire(False)
@@ -176,12 +157,11 @@ def execution(mode,lock,mlock,command,opr,start,end,stack,address,value,tablecou
                 elif mode=='1':
                     lock.release()
         endflag.value=1
+    #backward mode
     elif args[2]=='b':
-        #print("parflag="+str(parpath)+"")
         while pc<end:
             if parpath!=0:
                 lock.acquire()
-            #if you want to measure time, comment out
             if args[3]!='q':
                 if command[pc]==0:
                     command1='    nop'
@@ -195,7 +175,7 @@ def execution(mode,lock,mlock,command,opr,start,end,stack,address,value,tablecou
                     command1='    par'
                 print("~~~~~~~~Process"+str(parpath)+" execute~~~~~~~~")
                 print("pc = "+str(pc+1)+"   command = "+command1+"   operand = "+str(opr[pc])+"")
-            (pc,pre,stack,top,rtop)=executedcommand(mlock,stack,rstack,lstack,command[pc],opr[pc],pc,pre,top,rtop,ltop,address,value,parpath)
+            (pc,pre,stack,top,rtop)=executedcommand(stack,rstack,lstack,command[pc],opr[pc],pc,pre,top,rtop,ltop,address,value,parpath)
             if args[3]!='q':
                 print("shared variable stack: "+str(value[0:tablecount])+"")
             if parpath!=0:
@@ -219,7 +199,7 @@ def coderead(start,end):
         s1=re.search(r'\d+',t1)
         t2=codes[i+2:i+8]
         s2=re.search(r'\d+',t2)
-        #if ((int)(s1.group())!=10 and parflag==0):
+        #comに命令oprに被演算子を格納
         com.append((int)(s1.group()))
         opr.append((int)(s2.group()))
         if ((int)(s1.group())==10 and (int)(s2.group())==0):
@@ -227,36 +207,7 @@ def coderead(start,end):
         elif ((int)(s1.group())==10 and (int)(s2.group())==1):
             end.append(count_pc)
             parflag=parflag+1
-        #elif ((int)(s1.group())==10 and parflag==2 and (int)(s2.group())==0):
-        #   start2=count_pc
-        #elif ((int)(s1.group())==10 and parflag==2 and (int)(s2.group())==1):
-        #    end2=count_pc
-        #elif ((int)(s1.group())==10 and (int)(s2.group())==0 and parflag==0):
-        #    parflag=1
-        #    parcom1.append(10)
-        #    paropr1.append(0)
-        #elif ((int)(s1.group())==10 and (int)(s2.group())==1 and parflag==1):
-        #    parflag=2
-        #    parcom1.append(10)
-        #    paropr1.append(1)
-        #elif ((int)(s1.group())==10 and (int)(s2.group())==0 and parflag==2):
-        #    parcom2.append(10)
-        #    paropr2.append(0)
-        #elif ((int)(s1.group())==10 and (int)(s2.group())==1 and parflag==2):
-        #    parflag=0
-        #    parcom2.append(10)
-        #    paropr2.append(1)
-        #elif ((int)(s1.group())!=10 and parflag==1):
-        #    parcom1.append((int)(s1.group()))
-        #    paropr1.append((int)(s2.group()))
-        #elif ((int)(s1.group())!=10 and parflag==2):
-        #    parcom2.append((int)(s1.group()))
-        #    paropr2.append((int)(s2.group()))
-        #print(com[count_pc],end=',')
-        #print(opr[count_pc],end='\n')
         count_pc=count_pc+1
-    #print(com)
-    #print(opr)
     return (start,end)
     
 
@@ -274,13 +225,10 @@ def backward():
         f7=open(path7,mode='r')
         temp=f4.read()
         ldata=re.findall(r'[-]?\d+',temp)
-        #print(ldata)
         temp=f5.read()
         rdata=re.findall(r'[-]?\d+',temp)
-        #print(rdata)
         temp=f7.read()
         stack=re.findall(r'[-]?\d+',temp)
-        #print(stack)
         f4.close()
         f5.close()
         f7.close()
@@ -324,10 +272,6 @@ def forward(ltop,rtop):
 
 if __name__ == '__main__':
     start_time = time.time()
-    #start1=0
-    #end1=0
-    #start2=0
-    #end2=0
     start=[]
     end=[]
     tabledata=[]
@@ -350,48 +294,47 @@ if __name__ == '__main__':
     tabledata=f.read()
     f.close()
     k=0
+    #変数名と変数の値との関係を保存するための機能現在の処理には必要ない将来用
     for i in range(0,len(tabledata),20):
         t=tabledata[i+11:i+13]
         s=re.search(r'\d+',t)
-        #print(s.group())
         address[k]=((int)(s.group()))
         t2=tabledata[i+13:i+19]
         s2=re.search(r'\d+',t2)
-        #print(s2.group())
         value[k]=((int)(s2.group()))
         k=k+1
         tablecount=tablecount+1
 
     backward()
 
+    #forward mode
     if args[2]=='f':
         (start,end)=coderead(start,end)
         for i in range(0,parflag,1):
             endflag[i] = Value('i',0)
-            #print(endflag[i].value)
             lock[i] = Lock()
-        #print(start)
-        #print(end)
         if parflag!=0:
             if args[3]=='q':
                 mode='1'
             else:
                 mode=input('mode   1:auto 2:select >> ')
+            #逐次実行の部分を実行
             stack=execution(mode,lockfree,lockfree,com,opr,0,start[0],stack,address,value,tablecount,rstack,lstack,rtop,ltop,endflag0,0)
-            #monitor=Process(target=monitor,args=(lock1,lock2,mlock))
             if mode=='2':
                 for i in range(0,parflag,1):
                     lock[i].acquire()
                 process={}
+                #並列プロセスの生成
                 for i in range(0,parflag,1):        
                     process[i]=Process(target=execution,args=(mode,lock[i],mlock,com,opr,start[i],end[i],stack,address,value,tablecount,rstack,lstack,rtop,ltop,endflag[i],i+1))
-            #process2=Process(target=execution,args=(mode,lock2,mlock,com,opr,start,end,stack,address,value,rstack,lstack,rtop,ltop,endflag2,2))
             if mode=='1':
                 process={}
+                #並列プロセスの生成
                 for i in range(0,parflag,1):        
                     process[i]=Process(target=execution,args=(mode,lock[0],mlock,com,opr,start[i],end[i],stack,address,value,tablecount,rstack,lstack,rtop,ltop,endflag[i],i+1))
             for i in range(0,parflag,1):
                     process[i].start()
+            #モニター: メインプロセスでサブプロセスの実行を管理する
             if mode=='2':
                 while a!='esc':
                     a=input('>> ')
@@ -401,27 +344,18 @@ if __name__ == '__main__':
                         if a==str(i+1) and endflag[i].value!=1 and ifflag==0:
                             ifflag=1
                             lock[i].release()
-                        #elif a=='2' and endflag2.value!=1:
-                        #    lock2.release()
             for i in range(0,parflag,1):
                 process[i].join()
         elif parflag==0:
             mode='1'
             execution(mode,lockfree,mlock,com,opr,0,len(com),stack,address,value,tablecount,rstack,lstack,rtop,ltop,endflag0,0)
-        #print(value[0:tablecount])
-        #print(lstack[0:ltop.value])
-        #print(rstack[0:rtop.value])
         forward(ltop.value,rtop.value)
 
     elif args[2]=='b':
         (start,end)=coderead(start,end)
         for i in range(0,parflag,1):
             endflag[i] = Value('i',0)
-            #print(endflag[i].value)
             lock[i] = Lock()
-        #print(""+str(start1)+" "+str(end1)+" "+str(start2)+" "+str(end2)+"")
-        #print(ldata)
-        #print(rdata)
         for i in range(0,len(ldata),1):
             lstack[i]=int(ldata[i])
             ltop.value=ltop.value+1
@@ -431,10 +365,8 @@ if __name__ == '__main__':
         for i in range(0,len(stack),1):
             value[i]=int(stack[i])
         ltop.value=ltop.value-1
-        #print(value[:])
         rtop.value=rtop.value-1
         mode='0'
-        #print(str(parflag))
         if parflag!=0:
             #______measure time mode_________ 
             if args[3]=='q':
@@ -443,11 +375,10 @@ if __name__ == '__main__':
                 mode=input('mode   1:auto  2:select >> ')
             for i in range(0,parflag,1):
                 lock[i].acquire()
-            #monitor=Process(target=monitor,args=(lock1,lock2,mlock))
             process={}
+            #並列プロセスの生成
             for i in range(0,parflag,1):
                 process[i]=Process(target=execution,args=(mode,lock[parflag-i-1],mlock,com,opr,end[i],start[i],stack,address,value,tablecount,rstack,lstack,rtop,ltop,endflag[i],parflag-i))
-            #process2=Process(target=execution,args=(mode,lock2,mlock,com,opr,start2,end2,stack,address,value,rstack,lstack,rtop,ltop,endflag1,1))
             for i in range(0,parflag,1):
                 process[i].start()
             a='2'
@@ -472,13 +403,13 @@ if __name__ == '__main__':
                             endcount=endcount+1
                     if endcount==parflag:
                         a='esc'
-            #monitor.start()
-            #monitor.join()
             for i in range(0,parflag,1):
                 process[i].join()
+            #逐次実行の部分を実行
             execution(mode,lockfree,lockfree,com,opr,start[parflag-1]+1,count_pc,stack,address,value,tablecount,rstack,lstack,rtop,ltop,endflag0,0)
         elif parflag==0:
             execution(mode,lockfree,lockfree,com,opr,0,len(com),stack,address,value,tablecount,rstack,lstack,rtop,ltop,endflag0,0)
 
+    #経過時間の表示
     elapsed_time = time.time()-start_time
     print("elapsed_time:{0}".format(elapsed_time) + "[sec]")
